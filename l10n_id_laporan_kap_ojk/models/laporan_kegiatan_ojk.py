4da172fb1af5a566eb56e9731596f88613e702c9
mikevhe18 <viriyananda.michael@gmail.com>
2021-05-12 19:47:52 +0700

    [l10n_id_laporan_kap_ojk] 8.0.1.0.0

---
+++ l10n_id_laporan_kap_ojk/models/laporan_kegiatan_ojk.py
@@ -0,0 +1,274 @@
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import _, api, fields, models
+from openerp.exceptions import Warning as UserError
+
+
+class AccountantLaporanKegiatanOJK(models.Model):
+    _name = "accountant.laporan_kegiatan_ojk"
+    _description = "Laporan Kegiatan OJK"
+    _inherit = [
+        "mail.thread",
+        "ir.needaction_mixin",
+        "tier.validation",
+        "base.sequence_document",
+        "base.workflow_policy_object",
+        "base.cancel.reason_common",
+    ]
+    _state_from = ["draft", "confirm"]
+    _state_to = ["valid"]
+
+    @api.model
+    def _default_company_id(self):
+        return self.env.user.company_id.id
+
+    @api.multi
+    def _compute_policy(self):
+        _super = super(AccountantLaporanKegiatanOJK, self)
+        _super._compute_policy()
+
+    name = fields.Char(
+        string="# Document",
+        default="/",
+        required=True,
+        copy=False,
+        readonly=True,
+        states={
+            "draft": [
+                ("readonly", False),
+            ],
+        },
+    )
+    company_id = fields.Many2one(
+        string="Company",
+        comodel_name="res.company",
+        required=True,
+        default=lambda self: self._default_company_id(),
+        readonly=True,
+        states={
+            "draft": [
+                ("readonly", False),
+            ],
+        },
+    )
+    date_report = fields.Date(
+        string="Report Date",
+        required=True,
+        readonly=True,
+        states={
+            "draft": [
+                ("readonly", False),
+            ],
+        },
+    )
+    date_start = fields.Date(
+        string="Start Date",
+        required=True,
+        readonly=True,
+        states={
+            "draft": [
+                ("readonly", False),
+            ],
+        },
+    )
+    date_end = fields.Date(
+        string="End Date",
+        required=True,
+        readonly=True,
+        states={
+            "draft": [
+                ("readonly", False),
+            ],
+        },
+    )
+    accountant_report_ids = fields.Many2many(
+        string="Accountant Reports",
+        comodel_name="accountant.report",
+        relation="rel_accountant_ojk_accountant_report_ids",
+        column1="ojk_id",
+        column2="accountant_report_id",
+        readonly=True,
+        states={
+            "draft": [
+                ("readonly", False),
+            ],
+        },
+    )
+    pimpinan_rekan_id = fields.Many2one(
+        string="Pimpinan Rekan",
+        comodel_name="res.partner",
+        required=True,
+        readonly=True,
+        states={
+            "draft": [
+                ("readonly", False),
+            ],
+        },
+    )
+    note = fields.Text(
+        string="Note",
+    )
+    state = fields.Selection(
+        string="State",
+        selection=[
+            ("draft", "Draft"),
+            ("confirm", "Waiting for Approval"),
+            ("valid", "Valid"),
+            ("cancel", "Cancelled"),
+        ],
+        copy=False,
+        default="draft",
+        required=True,
+        readonly=True,
+    )
+    confirm_date = fields.Datetime(
+        string="Confirmation Date",
+        readonly=True,
+        copy=False,
+    )
+    confirm_user_id = fields.Many2one(
+        string="Confirmed By",
+        comodel_name="res.users",
+        readonly=True,
+        copy=False,
+    )
+    valid_date = fields.Datetime(
+        string="Validate Date",
+        readonly=True,
+        copy=False,
+    )
+    valid_user_id = fields.Many2one(
+        string="Validate By",
+        comodel_name="res.users",
+        readonly=True,
+        copy=False,
+    )
+    cancel_date = fields.Datetime(
+        string="Cancel Date",
+        readonly=True,
+        copy=False,
+    )
+    cancel_user_id = fields.Many2one(
+        string="Cancelled By",
+        comodel_name="res.users",
+        readonly=True,
+        copy=False,
+    )
+    # Policy Field
+    confirm_ok = fields.Boolean(
+        string="Can Confirm",
+        compute="_compute_policy",
+    )
+    restart_approval_ok = fields.Boolean(
+        string="Can Restart Approval",
+        compute="_compute_policy",
+    )
+    restart_ok = fields.Boolean(
+        string="Can Restart",
+        compute="_compute_policy",
+    )
+    cancel_ok = fields.Boolean(
+        string="Can Cancel",
+        compute="_compute_policy",
+    )
+
+    @api.multi
+    def action_confirm(self):
+        for record in self:
+            record.write(record._prepare_confirm_data())
+            record.request_validation()
+
+    @api.multi
+    def action_valid(self):
+        for record in self:
+            record.write(record._prepare_valid_data())
+
+    @api.multi
+    def action_cancel(self):
+        for record in self:
+            record.write(record._prepare_cancel_data())
+
+    @api.multi
+    def action_restart(self):
+        for record in self:
+            record.write(record._prepare_restart_data())
+
+    @api.multi
+    def _prepare_confirm_data(self):
+        self.ensure_one()
+        return {
+            "state": "confirm",
+            "confirm_date": fields.Datetime.now(),
+            "confirm_user_id": self.env.user.id,
+        }
+
+    @api.multi
+    def _prepare_valid_data(self):
+        self.ensure_one()
+        sequence = self._create_sequence()
+        return {
+            "state": "valid",
+            "name": sequence,
+            "valid_date": fields.Datetime.now(),
+            "valid_user_id": self.env.user.id,
+        }
+
+    @api.multi
+    def _prepare_cancel_data(self):
+        self.ensure_one()
+        return {
+            "state": "cancel",
+            "cancel_date": fields.Datetime.now(),
+            "cancel_user_id": self.env.user.id,
+        }
+
+    @api.multi
+    def _prepare_restart_data(self):
+        self.ensure_one()
+        return {
+            "state": "draft",
+            "confirm_date": False,
+            "confirm_user_id": False,
+            "valid_date": False,
+            "valid_user_id": False,
+            "cancel_date": False,
+            "cancel_user_id": False,
+        }
+
+    @api.multi
+    def unlink(self):
+        strWarning = _("You can only delete data on draft state")
+        for record in self:
+            if record.state != "draft":
+                if not self.env.context.get("force_unlink", False):
+                    raise UserError(strWarning)
+        _super = super(AccountantLaporanKegiatanOJK, self)
+        _super.unlink()
+
+    @api.multi
+    def validate_tier(self):
+        _super = super(AccountantLaporanKegiatanOJK, self)
+        _super.validate_tier()
+        for record in self:
+            if record.validated:
+                record.action_valid()
+
+    @api.multi
+    def restart_validation(self):
+        _super = super(AccountantLaporanKegiatanOJK, self)
+        _super.restart_validation()
+        for record in self:
+            record.request_validation()
+
+    @api.multi
+    def name_get(self):
+        result = []
+        for record in self:
+            if record.name == "/":
+                name = "*" + str(record.id)
+            else:
+                name = record.name
+            result.append((record.id, name))
+        return result
