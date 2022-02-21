16ef71eb2539e2c0e0f9e8f96318204ce7e249d3
Asrulbastian <asrulbastianyunas@gmail.com>
2022-02-21 23:00:30 +0700

    [l10n_id_accountant_report_stakeholder_report_bni] pre-commit fix

--- l10n_id_accountant_report_stakeholder_report_bni/__openerp__.py
+++ l10n_id_accountant_report_stakeholder_report_bni/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 # pylint: disable=locally-disabled, manifest-required-author
 {
-    "name": "Accountant Report - BNI Stakeholder Report",
+    "name": "BNI Stakeholder Report for Accountant Report",
     "version": "8.0.1.0.0",
     "author": "PT. Simetri Sinergi Indonesia,OpenSynergy Indonesia",
     "website": "https://simetri-sinergi.id",
