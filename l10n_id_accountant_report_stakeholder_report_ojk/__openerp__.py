fee6fa37c1a89778671e66d0288b60a6d74fc755
Andhitia Rama <andhitia.r@gmail.com>
2022-02-25 06:24:06 +0700

    [l10n_id_accountant_report_stakeholder_report_ojk] 8.0.1.0.1
    * Add tier_definition

--- l10n_id_accountant_report_stakeholder_report_ojk/__openerp__.py
+++ l10n_id_accountant_report_stakeholder_report_ojk/__openerp__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "OJK Stakeholder Report for Accountant Report",
-    "version": "8.0.1.0.0",
+    "version": "8.0.1.0.1",
     "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
     "website": "https://simetri-sinergi.id",
     "license": "AGPL-3",
